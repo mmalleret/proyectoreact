800ebdb25fb5dd55367828b329f7f0b4c8e895e7
zoeHamraa <65314353+zoeHamraa@users.noreply.github.com>
2021-03-26 19:33:26 -0300

    Errores footer y footer en app.js

--- proyectoreact/src/App.js
+++ proyectoreact/src/App.js
@@ -16,7 +16,32 @@ function App() {
     <div className="uk-slider-container-offset" uk-slider="true" >
         <Carrusel/>
     </div>
-
+  
+     {/* Footer */}
+    <br/>
+    <footer className="page-footer">
+        <div className="container">
+            <div className="uk-flex-middle" uk-grid="true" >
+                <div className="uk-width-2-3@m">
+                    <p></p>
+                </div>
+                <div className="uk-width-1-3@m uk-flex-first">
+                  <h5>Contactate con Nosotras</h5>
+                <ul className="icons uk-column-1-2 uk-column-divider">
+                    <li><span><i className="fab fa-facebook"> ConnectIN</i></span></li>
+                    <li><span><i className="fab fa-twitter"> ConnectIN.app</i></span></li>
+                    <li><span><i className="fab fa-instagram"> ConnectIN</i></span></li>
+                    <li><span><i className="fab fa-linkedin"> ConnectIN</i></span></li>
+                </ul>  
+                </div>
+            </div>
+        </div>
+        <div className="footer-copyright">
+          <div className="container">
+            @ConnectIN Season I
+          </div>
+        </div>
+      </footer>
 
 
   </div>
