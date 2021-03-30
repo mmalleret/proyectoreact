aab54438e7d5095e29c0b234520cc30bd694ed55
Mmalleret <milimalleret1@gmail.com>
2021-03-30 17:20:10 -0300

    Tarjetas

--- proyectoreact/src/components/Itemcarrusel.js
+++ proyectoreact/src/components/Itemcarrusel.js
@@ -1,23 +1,17 @@
 function ItemCarrusel(props) {
     return(
-        <li>
-            <div className={"uk-child-width-1-1@m"} uk-grid="true" >
-                <div>
-                    <div className="uk-card uk-card-default">
-                        <div className="uk-card-media-top">
-                            <img src={props.image} alt=""/>
-                        </div>
-                        <div className="uk-card-body">
-                            <h3 className="uk-card-title">{props.fullname + " (" + props.edad + ")"}</h3>
-                            <ul>
-                                <li>{props.mail}</li>
-                                <li>{props.fecha}</li>
-                            </ul>
-                        </div>
+        <div>
+            <div className="uk-card uk-card-default uk-card-body" id='tarjeta'>
+                    <div className="uk-card-media-top">
+                        <img src={props.src} alt="" width="250" height="250"/>
                     </div>
-                </div>
+                    <div className="uk-card-body">
+                        <h3 className="uk-card-title">{props.fullname} - {props.edad}</h3>
+                        <p>{props.fecha}</p>
+                        <p>{props.mail}</p>
+                    </div>
             </div>
-        </li>
+        </div>
         );
 }
 export default ItemCarrusel;
